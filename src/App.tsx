87828287c08b1c229ab97de8fd7b876e0f903605
Hugo Cruz <hugocruzlfc@gmail.com>
2024-10-29 16:52:36 +0100

    feat: add calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { DatePickerRange } from "./components/DatePickerRange";
+
 function App() {
-  return <main>hi</main>;
+  return (
+    <main className="flex space-y-10 flex-col py-10 px-10 items-center">
+      <h1 className="text-xl font-bold text-gray-800">
+        Add{" "}
+        <a
+          href="https://ui.shadcn.com/docs/installation/vite"
+          className="text-blue-600 underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Shadcn/ui
+        </a>{" "}
+        to Vite.
+      </h1>
+      <DatePickerRange />
+    </main>
+  );
 }
 
 export default App;
